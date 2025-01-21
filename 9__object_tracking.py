e0063e9bbf942b0ee2007d3f990aa218e2d97b2c
Jung Eun Ji <ejjung312@naver.com>
2025-01-21 15:16:18 +0900

    객체 검출

--- 9__object_tracking.py
+++ 9__object_tracking.py
@@ -5,6 +5,10 @@ def select_tracker(tracker_type):
     """
     객체 추적 알고리즘
     
+    속도가 중요한 경우: MOSSE, KCF.
+    정밀도가 중요한 경우: CSRT, MIL.
+    객체 재탐지가 필요한 경우: TLD.
+    움직임이 적은 경우: MedianFlow.
     """
     if tracker_type == 'BOOSTING':
         return cv2.legacy.TrackerBoosting_create()
